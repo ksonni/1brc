8645b3081839ca239020479938a77a7b20551ee7
Keshav Sonni <keshav.vm97@gmail.com>
2024-03-24 00:44:55 +0000

    do the data read by partitioning the file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module 1brc
 
 go 1.22.1
+
+require github.com/inhies/go-bytesize v0.0.0-20220417184213-4913239db9cf // indirect
--- main.go
+++ main.go
@@ -3,44 +3,115 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
+	"math"
 	"os"
 	"time"
+
+	"github.com/inhies/go-bytesize"
 )
 
-const nLinesForEcho = 20_000_000
 const filePath = "./data/measurements.txt"
 
-// 24.6 seconds base case without any processing on an M1 MacbookPro
+const maxMemoryBytes int64 = 4 * 1024 * 1024 * 1024
+const maxChannels int64 = 8
+
+// 6.3 seconds base case without any processing on an M1 MacbookPro
 func main() {
 	start := time.Now()
-	nLines := 0
-
-	printProgress := func() {
-		fmt.Printf("Processed %d lines, time elapsed: %fs\n",
-			nLines, time.Now().Sub(start).Seconds())
-	}
 
 	file, err := os.OpenFile(filePath, os.O_RDONLY, os.ModePerm)
 	if err != nil {
-		log.Fatalf("main: failed to open file: %v", err)
+		log.Fatalf("failed to open file: %v", err)
 		return
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		_ = scanner.Text()
-		nLines += 1
-		if nLines%nLinesForEcho == 0 {
-			printProgress()
+	parts, err := partition(file)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+    fmt.Printf("Processing %d partitions\n", len(*parts))
+	for i, part := range *parts {
+        fmt.Printf("Processing partition %d\n", i)
+		x, err := processPartition(file, part)
+		if x == nil || err != nil {
+			log.Fatalf("%v", err)
 		}
+		fmt.Printf("time elapsed: %fs\n", time.Now().Sub(start).Seconds())
+	}
+}
+
+type Partition struct {
+	start  int64
+	length int64
+}
+
+// Partitions a large file into smaller chunks using file seeking.
+// Gaurantees no chunk has incomplete lines.
+func partition(file *os.File) (*[]Partition, error) {
+	stat, err := file.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("failed to stat file: %v", err)
+	}
+	size := stat.Size()
+	fmt.Printf("Size of file: %s\n", formatBytes(size))
+	fmt.Printf("Memory limit: %s\n", formatBytes(maxMemoryBytes))
+
+	partSize := int64(math.Min(
+		float64(size/maxChannels),
+		float64(maxMemoryBytes/maxChannels),
+	))
+	fmt.Printf("Target size per partition: %s\n", formatBytes(partSize))
+
+	var position int64
+	positions := []int64{0}
+	for {
+		nextPos := position + partSize
+		if nextPos >= size-1 {
+			break
+		}
+		_, err := file.Seek(nextPos, 0)
+		if err != nil {
+			return nil, fmt.Errorf("seek failed when partitioning file: %v", err)
+		}
+		rd := bufio.NewReader(file)
+		line, err := rd.ReadString('\n')
+		if err != nil {
+			return nil, fmt.Errorf("read failed when partitioning file: %v", err)
+		}
+		lineBytes := len([]byte(line))
+		nextPos += int64(lineBytes)
+		positions = append(positions, nextPos)
+		position = nextPos
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("main: failed to scan file: %v", err)
-		return
+	nPositions := len(positions)
+	partitions := make([]Partition, nPositions)
+	for i := range positions {
+		if i != nPositions-1 {
+			partitions[i] = Partition{start: positions[i], length: positions[i+1]-positions[i]}
+		} else {
+			partitions[i] = Partition{start: positions[i], length: size - positions[i]}
+		}
 	}
-	printProgress()
+	return &partitions, nil
+}
+
+func processPartition(file *os.File, part Partition) (*string, error) {
+	if _, err := file.Seek(part.start, 0); err != nil {
+		return nil, fmt.Errorf("seek failed when processing partition: %v", err)
+	}
+	out := make([]byte, part.length)
+	if _, err := io.ReadFull(file, out); err != nil {
+		return nil, fmt.Errorf("read failed when processing partition: %v", err)
+	}
+	s := string(out)
+	return &s, nil
+}
+
+func formatBytes(size int64) string {
+	return bytesize.New(float64(size)).String()
 }
 
