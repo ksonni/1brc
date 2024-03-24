701b2a21556636e025559371c5e5f003e71eb9e8
Keshav Sonni <keshav.vm97@gmail.com>
2024-03-24 19:23:47 +0000

    bit of clean up

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ import (
 )
 
 const kFilePath = "./data/measurements.txt"
-const kMaxMemroyBytes int64 = 256 * 1024 * 1024
+const kPartitionSizeBytes int64 = 32 * 1024 * 1024
 const kMaxChannels int64 = 8
+const kExpectedResults = 10_000
 
 func main() {
 	start := time.Now()
@@ -20,63 +21,63 @@ func main() {
 		log.Fatalf("Failed to read file: %v", err)
 	}
 	defer file.Close()
-    
-    out := processConcurrentByPartition(file) 
+
+	out := processConcurrentByPartition(file)
 
 	fmt.Printf("Got %d entries in result\n", len(out))
 
 	fmt.Printf("Time elapsed: %fs\n", time.Now().Sub(start).Seconds())
 }
 
-// 21s on Macbook Pro M1 8 core
+// 19s on Macbook Pro M1 8 core
 func processConcurrentByPartition(file *os.File) map[string]*Summary {
-	parts, err := calcPartitions(file, kMaxMemroyBytes, kMaxChannels)
+	// Break down the task into managable partitions
+	parts, err := calcPartitions(file, kPartitionSizeBytes)
 	if err != nil {
 		log.Fatalf("Failed to calculate file partitions: %v", err)
 	}
 	fmt.Printf("Processing concurrently by partition: %d parts\n", len(*parts))
 
-	// counting semaphore to limit concurrency
-	tokens := make(chan struct{}, kMaxChannels*5)
-
-	var wg sync.WaitGroup
-
-	reusltsCh := make(chan map[string]*Summary, 1000)
+	// Channel to aggregate results
+	results := make(map[string]*Summary)
+	reusltsCh := make(chan map[string]*Summary, kExpectedResults)
 	reusltsComplete := make(chan struct{})
-	out := make(map[string]*Summary)
 	go func() {
 		for res := range reusltsCh {
 			for k, summary := range res {
-                oSummmary, exists := out[k]
-                if !exists {
-                    out[k] = summary
-                    continue
-                }
+				oSummmary, exists := results[k]
+				if !exists {
+					results[k] = summary
+					continue
+				}
 				oSummmary.total += summary.total
 				oSummmary.count += summary.count
-				oSummmary.min += min(oSummmary.min, summary.min)
-				oSummmary.max += max(oSummmary.max, summary.max)
+				oSummmary.min = min(oSummmary.min, summary.min)
+				oSummmary.max = max(oSummmary.max, summary.max)
 			}
 		}
 		close(reusltsComplete)
 	}()
 
+	// Worker channels to process data
+	tokens := make(chan struct{}, kMaxChannels)
+	var wg sync.WaitGroup
 	for i, part := range *parts {
 		tokens <- struct{}{} // Acquire capacity
 		wg.Add(1)
-        data, err := readPartition(file, part)
-        if err != nil {
-            log.Fatalf("Failed to read file partition: %v", err)
-        }
-		go func(dat *string, index int) {
-			processPartition(dat, index, reusltsCh)
+		data, err := readPartition(file, part)
+		if err != nil {
+			log.Fatalf("Failed to read file partition: %v", err)
+		}
+		go func(d *string, index int) {
+			processPartition(d, index, reusltsCh)
 			<-tokens // Release
 			wg.Done()
 		}(data, i)
 	}
 	wg.Wait()
+
 	close(reusltsCh)
 	<-reusltsComplete
-	return out
+	return results
 }
-
--- partition.go
+++ partition.go
@@ -3,11 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"hash/fnv"
 	"io"
-	"math"
 	"os"
-	"strings"
 )
 
 type Partition struct {
@@ -15,21 +12,25 @@ type Partition struct {
 	length int64
 }
 
+type Summary struct {
+	total int64
+	count int64
+	min   int64
+	max   int64
+}
+
 // Calculates partitions of a target size ensuring that each partition ends with a newline.
 // Uses seeking to perform partitioning without actually reading all the contents of the file.
-func calcPartitions(file *os.File, maxMemory int64, targetParts int64) (*[]Partition, error) {
+func calcPartitions(file *os.File, maxPartSize int64) (*[]Partition, error) {
 	stat, err := file.Stat()
 	if err != nil {
 		return nil, err
 	}
 	size := stat.Size()
 	fmt.Printf("Size of file: %s\n", formatBytes(size))
-	fmt.Printf("Memory limit: %s\n", formatBytes(maxMemory))
+	fmt.Printf("Memory limit: %s\n", formatBytes(maxPartSize))
 
-	partSize := int64(math.Min(
-		float64(size/targetParts),
-		float64(maxMemory/targetParts),
-	))
+	partSize := min(size, maxPartSize)
 	fmt.Printf("Target size per partition: %s\n", formatBytes(partSize))
 
 	var position int64
@@ -80,62 +81,10 @@ func readPartition(file *os.File, part Partition) (*string, error) {
 
 func processPartition(data *string, index int, ch chan<- map[string]*Summary) {
 	counts := make(map[string]*Summary)
-	processPartitionMap2(data, counts, index, ch)
+	processPartitionMap(data, counts, index, ch)
 }
 
-func processPartitionMap(data *string, counts map[string]int64, index int, ch chan<- map[string]int64) {
-	sc := bufio.NewScanner(strings.NewReader(*data))
-	lines := 0
-	for sc.Scan() {
-		line := sc.Text()
-		if line == "" {
-			continue
-		}
-		lines += 1
-
-		var val int64
-		var multiplier int64 = 1
-		parsingNum := false
-		var colonInd int
-		for i, ch := range line {
-			if ch == ';' {
-				parsingNum = true
-				colonInd = i
-				continue
-			}
-			if !parsingNum {
-				continue
-			}
-			if ch == '-' {
-				multiplier = -1
-				continue
-			}
-			if ch == '.' {
-				continue
-			}
-			val = val*10 + int64(ch-'0')
-		}
-		counts[line[:colonInd]] += multiplier * val
-	}
-	ch <- counts
-	fmt.Printf("Processed partition %d, num lines (millions): %d\n", index, lines/1_000_000)
-}
-
-func hash(s string) uint32 {
-	h := fnv.New32a()
-	h.Write([]byte(s))
-	return h.Sum32()
-}
-
-type Summary struct {
-	total int64
-	count int64
-	min   int64
-	max   int64
-}
-
-// / Just to see what the thoretical time limit would be
-func processPartitionMap2(data *string, counts map[string]*Summary, index int, ch chan<- map[string]*Summary) {
+func processPartitionMap(data *string, counts map[string]*Summary, index int, ch chan<- map[string]*Summary) {
 	var lines int64
 	wordStart := 0
 	wordEnd := 0
@@ -152,23 +101,25 @@ func processPartitionMap2(data *string, counts map[string]*Summary, index int, c
 		case '.':
 			break
 		case '\n':
-            s := s[wordStart:wordEnd]
-            summary, exists := counts[s] 
-            if !exists {
-                sm := Summary{}
-                summary = &sm
-                counts[s] = summary
-            }
+			s := s[wordStart:wordEnd]
+			val := number * multiplier
 
-            val := int64(number * multiplier)
-            
-            summary.total +=  val
-            summary.count += 1
-            summary.min = min(summary.min, val)
-            summary.max = max(summary.min, val)
-			
-            // Reset for next iteration
-            inNum = false
+			if summary, exists := counts[s]; exists {
+				summary.total += val
+				summary.count += 1
+				if val < summary.min {
+					summary.min = val
+				}
+				if val > summary.max {
+					summary.max = val
+				}
+			} else {
+				sm := Summary{total: val, count: 1, min: val, max: val}
+				counts[s] = &sm
+			}
+
+			// Reset for next iteration
+			inNum = false
 			multiplier = 1
 			number = 0
 			wordStart = i + 1
@@ -178,21 +129,6 @@ func processPartitionMap2(data *string, counts map[string]*Summary, index int, c
 				number = number*10 + int64(ch-'0')
 			}
 		}
-	}
-	ch <- counts
-	fmt.Printf("Processed partition %d, num lines (millions): %d\n", index, lines/1_000_000)
-}
-
-// / Just to see what the time limit would be
-func processPartitionMapNoOp(data *string, counts map[string]int64, index int, ch chan<- map[string]int64) {
-	sc := bufio.NewScanner(strings.NewReader(*data))
-	lines := 0
-	for sc.Scan() {
-		line := sc.Text()
-		if line == "" {
-			continue
-		}
-		lines += 1
 	}
 	ch <- counts
 	fmt.Printf("Processed partition %d, num lines (millions): %d\n", index, lines/1_000_000)
